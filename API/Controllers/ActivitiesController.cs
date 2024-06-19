cc0dfbb715c76428b03c1b226b2b36bc0e4760fb
Pieter du Preez <pieterdp@mie.co.za>
2024-06-19 14:31:56 +0200

    Initial Commit 2

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,33 @@
+﻿using Domain;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using Persistence;
+
+namespace API.Controllers
+{
+    public class ActivitiesController : BaseApiController
+    {
+        private readonly DataContext _context;
+
+        public ActivitiesController(DataContext context)
+        {
+            _context=context;
+        }
+
+        [HttpGet]// api/activities
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await _context.Activities.ToListAsync();
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivity(Guid Id)
+        {
+            return await _context.Activities.FindAsync(Id);
+        }
+    }
+}
